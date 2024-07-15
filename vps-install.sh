c5ac76cc1d5531e107a9642e4b400195fc5c5fb7
tafkat <jellerules@hotmail.com>
2024-07-15 21:32:36 +0200

    Update vps-install.sh

--- vps-install.sh
+++ vps-install.sh
@@ -2,8 +2,8 @@
 echo "Installing unzip"
 sudo apt install unzip -y
 echo "Fetching latest Hemis test version"
-wget --quiet https://github.com/Hemis-Blockchain/Hemis/releases/download/GMTest/Hemis-Linux.zip && sudo unzip Hemis-Linux.zip -d /usr/local/bin
-wget --quiet https://github.com/Hemis-Blockchain/Hemis/releases/latest/download/Hemis-params.zip && unzip Hemis-params.zip -d ~/.Hemis-params
+wget --quiet https://github.com/Hemis-Blockchain/Hemis/releases/latest/download/Hemis-Linux.zip && sudo unzip Hemis-Linux.zip -d /usr/local/bin
+wget --quiet https://github.com/Hemis-Blockchain/Hemis/releases/download/1.1.6/Hemis-params.zip && unzip Hemis-params.zip -d ~/.Hemis-params
 echo "Hemis succesfully installed. Creating data folder and config file"
 mkdir -p ~/.Hemis
 printf '%s\n' 'daemon=1' 'testnet=1' 'rpcuser=' 'rpcpassword=' > ~/.Hemis/Hemis.conf

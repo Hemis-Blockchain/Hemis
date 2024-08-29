ada84de42b48118c3659b64d0531ed3a45651e20
hypermoist <jacobms109@gmail.com>
2024-08-30 00:20:43 +1200

    Fixes..

--- src/gamemasterconfig.h
+++ src/gamemasterconfig.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2014-2015 The Dash developers
-// Copyright (c) 2015-2020 The Hemis Core developers
+// Copyright (c) 2015-2019 The PIVX developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
@@ -21,27 +21,27 @@ public:
     private:
         std::string alias;
         std::string ip;
-        std::string privKeyStr;
-        std::string pubKeyStr;
+        std::string privkeyStr;
+        std::string pubkeyStr;
         std::string txHash;
         std::string outputIndex;
 
     public:
         CGamemasterEntry(std::string& _alias,
                          std::string& _ip,
-                         std::string& _privKeyStr,
-                         std::string& _pubKeyStr,
+                         std::string& _privkeyStr,
+                         std::string& _pubkeyStr,
                          std::string& _txHash,
                          std::string& _outputIndex) :
-            alias(_alias), ip(_ip), privKeyStr(_privKeyStr), pubKeyStr(_pubKeyStr), txHash(_txHash), outputIndex(_outputIndex) { }
+            alias(_alias), ip(_ip), privkeyStr(_privkeyStr), pubkeyStr(_pubkeyStr), txHash(_txHash), outputIndex(_outputIndex) { }
 
-        std::string& getAlias() const { return alias; }
-        std::string& getOutputIndex() const { return outputIndex; }
+        std::string getAlias() const { return alias; }
+        std::string getOutputIndex() const { return outputIndex; }
         bool castOutputIndex(int& n) const;
-        std::string& getPrivKey() const { return privKeyStr; }
-        std::string& getPubKey() const { return pubKeyStr; }
-        std::string& getTxHash() const { return txHash; }
-        std::string& getIp() const { return ip; }
+        std::string getPrivKey() const { return privkeyStr; }
+        std::string getPubKeyStr() const { return pubkeyStr; }
+        std::string getTxHash() const { return txHash; }
+        std::string getIp() const { return ip; }
     };
 
     CGamemasterConfig() { entries = std::vector<CGamemasterEntry>(); }
